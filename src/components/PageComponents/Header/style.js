ba68a311f3ef73454c7cabd82319e64b02c9c135
ez0603 <dlwldjs3132@naver.com>
2024-10-11 20:26:53 +0900

    Feat : 사이드바 구현

--- src/components/PageComponents/Header/style.js
+++ src/components/PageComponents/Header/style.js
@@ -19,3 +19,103 @@ export const menuButton = css`
   color: #333;
   cursor: pointer;
 `;
+
+export const categoryLayout = css`
+  width: 100%;
+  position: relative;
+  top: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: orange;
+`;
+
+export const category = css`
+  width: 90%;
+  height: 50px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  background-color: aqua;
+  overflow-x: hidden;
+  white-space: nowrap;
+
+  @media (max-width: 700px) {
+    overflow-x: auto;
+    white-space: nowrap;
+  }
+`;
+
+export const list = css`
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 50px;
+  padding: 0;
+  margin: 0;
+
+  li {
+    cursor: pointer;
+  }
+
+  @media (max-width: 700px) {
+    flex-wrap: nowrap;
+  }
+
+  li {
+    cursor: pointer;
+    display: inline-block;
+  }
+`;
+
+export const listItem = css`
+  padding: 10px;
+  position: relative;
+
+  ::after {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #dbdbdb;
+    visibility: hidden;
+    transition: all 0.3s ease-in-out;
+  }
+
+  :hover::after {
+    visibility: visible;
+    width: 100%;
+  }
+`;
+
+export const arrowButton = css`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0.5);
+  border: none;
+  color: white;
+  cursor: pointer;
+  padding: 5px;
+  z-index: 99;
+  background-color: transparent;
+
+  @media (min-width: 701px) {
+    display: block;
+  }
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const left = css`
+  left: 3%;
+`;
+
+export const right = css`
+  right: 10px;
+`;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,6 @@
 /** @jsxImportSource @emotion/react */
 // import * as s from "./style";
 import AdminPageLayout from "../../components/PageComponents/AdminPageLayout/AdminPageLayout";
-import SideBar from "../../components/PageComponents/SideBar/SideBar";
 
 function MainPage(props) {
   return (
